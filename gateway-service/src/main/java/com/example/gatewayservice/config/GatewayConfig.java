62f4870e4d58156a60d2a395f3b32dc633b867e3
zaferdemrkrn <105773056+zaferdemrkrn@users.noreply.github.com>
2025-04-15 02:13:52 +0300

    Add files via upload

---
+++ gateway-service/src/main/java/com/example/gatewayservice/config/GatewayConfig.java
@@ -0,0 +1,20 @@
+package com.example.gatewayservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestClient;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class GatewayConfig {
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+    @Bean
+    public RestClient.Builder restClientBuilder() {
+        return RestClient.builder();
+    }
+}
